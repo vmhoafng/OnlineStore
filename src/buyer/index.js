16130ddeb5d425af7ee08fd12526b7f7ff52801e
vmhoafng <vominhhoang044@gmail.com>
2022-11-14 17:02:05 +0700

    commit

--- src/buyer/index.js
+++ src/buyer/index.js
@@ -4,12 +4,31 @@ import defaultProductList from "../admin/list/productList.js";
 let productList =
   JSON.parse(localStorage.getItem(PRODUCT_LIST)) ?? defaultProductList;
 localStorage.setItem(PRODUCT_LIST, JSON.stringify(productList));
-let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 let userStatus = JSON.parse(localStorage.getItem(currentUser));
-console.log(userStatus.cart);
 
-const addIntoCart = (index) => {
-  userStatus.cart = controllers.add(userStatus.cart, index);
+const addIntoCart = (product) => {
+  const existProduct = userStatus.cart.find((item) => item.id === product.id);
+  if (existProduct) {
+    userStatus.cart = controllers.update(userStatus.cart, existProduct.id, {
+      id: existProduct.id,
+      quantity: ++existProduct.quantity,
+    });
+  } else {
+    userStatus.cart = controllers.add(userStatus.cart, product);
+  }
+  localStorage.setItem(currentUser, JSON.stringify({ ...userStatus }));
+};
+const removeOutOfCart = (index) => {
+  const existProduct = userStatus.cart.find((item) => item.id === index);
+  if (existProduct && existProduct.quantity > 1) {
+    userStatus.cart = controllers.update(userStatus.cart, index, {
+      id: index,
+      quantity: --existProduct.quantity,
+    });
+  } else {
+    userStatus.cart = controllers.delete(userStatus.cart, index);
+  }
   localStorage.setItem(currentUser, JSON.stringify({ ...userStatus }));
 };
 // render
@@ -32,18 +51,18 @@ const render = () => {
           <span data-product="${product.id}" class="img">${product.id}<span/>
           <span data-product="${product.id}" class="type">${product.id}<span/>
           <button data-product="${product.id}" class="submit">Submit</button>
-          <button data-product="${product.id}" class="button">Delete</button>
         </div>
             `;
       }
     });
     document.querySelector("#app").innerHTML = newProductList.join("");
-    const cartList = productList.map((product) => {
-      if (userStatus.cart.includes(product.id)) {
+    const cartList = userStatus.cart.map((product) => {
+      if (userStatus) {
         return `
       <div>
-        <span data-product="${product.id}" class="id">${product.id}<span/>
-        <button data-product="${product.id}" class="button">Delete</button>
+        <span data-product="${product.id}" class="id">${product.id}</span>
+        <span data-product="${product.id}" class="quantity">${product.quantity}</span>
+        <button type="delete" data-product="${product.id}">Delete</button>
       </div>
           `;
       }
@@ -51,19 +70,48 @@ const render = () => {
     document.querySelector(".cart").innerHTML = cartList.join("");
     // handleEvents
     // add product
-    const handleAddProduct = () => {
+    const handleAddIntoCart = () => {
       const getBtn = document.querySelectorAll(".submit");
       getBtn.forEach((item) => {
         item.onclick = () => {
-          addIntoCart(item.dataset.product);
+          const getQuantity = document.querySelector(
+            `.quantity[data-product="${item.dataset.product}"]`
+          );
+          if (currentUser == null) {
+            alert("Use need to login to buy products");
+            return;
+          }
+          const product = {
+            id: item.dataset.product,
+            quantity: (getQuantity && getQuantity.innerHTML) || 1,
+          };
+          addIntoCart(product);
           loadItems();
         };
       });
     };
-    handleAddProduct();
+    handleAddIntoCart();
     // delete product
-    const handleDeleteProduct = () => {};
-    handleDeleteProduct();
+    const handleRemoveOutOfCart = () => {
+      const getBtn = document.querySelectorAll(
+        "button[data-product][type=delete]"
+      );
+      getBtn.forEach((element) => {
+        element.onclick = () => {
+          const getQuantity = document.querySelector(
+            `.quantity[data-product="${element.dataset.product}"]`
+          );
+          if (
+            getQuantity.innerHTML === "1" &&
+            !confirm("Bạn có muốn xoá sản phẩm khỏi giỏ hàng")
+          )
+            return;
+          removeOutOfCart(element.dataset.product);
+          loadItems();
+        };
+      });
+    };
+    handleRemoveOutOfCart();
   };
   loadItems();
   const listPage = () => {
