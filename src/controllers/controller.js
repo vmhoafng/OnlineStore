5ad0e5809772085ea2802b9b4f7be6636a6e626d
vmhoafng <vominhhoang044@gmail.com>
2022-11-29 06:28:40 +0700

    logic on update

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,22 +1,22 @@
 "use strict";
 
 const controllers = {
-  add([...list], data) {
+  add(list, data) {
     // spread List
-    let newList = [...list];
+    let newList = list;
     // add new data into ist
     newList.push(data);
     return newList;
   },
-  update([...list], updateId, data) {
+  update(list, updateId, data) {
     // spread List
-    let newList = [...list];
+    let newList = list;
     newList = newList.map((value) => (value.id === updateId ? data : value));
     return newList;
   },
-  delete([...list], id) {
+  delete(list, id) {
     // spread List
-    let newList = [...list];
+    let newList = list;
     newList = newList.filter((item) => item.id !== id);
     return newList;
   },
